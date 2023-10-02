e885551dbbb31e593d94afc046f305caeed49b9e
nfeldt2 <120221943+nfeldt2@users.noreply.github.com>
2023-10-02 11:42:34 -0500

    Create model.py

---
+++ model.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import math
+from einops import rearrange
+
+
+class AbsPosEnc(nn.Module):
+    r"""Inject some information about the relative or absolute position of the tokens
+        in the sequence. The positional encodings have the same dimension as
+        the embeddings, so that the two can be summed. Here, we use sine and cosine
+        functions of different frequencies.
+    .. math::
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=1024).
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=1024, scale_factor=1.0):
+        super(AbsPosEnc, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)  # positional encoding
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin((position * div_term)*(d_model/max_len))
+        pe[:, 1::2] = torch.cos((position * div_term)*(d_model/max_len))
+        pe = scale_factor * pe.unsqueeze(0)
+        self.register_buffer('pe', pe)  # this stores the variable in the state_dict (used for non-trainable variables)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        """
+        x = x + self.pe
+        return self.dropout(x)
+
+class AttentionWithRelPosEnc(nn.Module):
+    def __init__(self, emb_size, num_heads, seq_len, dropout):
+        super().__init__()
+        self.seq_len = seq_len
+        self.num_heads = num_heads
+        self.scale = emb_size ** -0.5
+        # self.to_qkv = nn.Linear(inp, inner_dim * 3, bias=False)
+        self.key = nn.Linear(emb_size, emb_size, bias=False)
+        self.value = nn.Linear(emb_size, emb_size, bias=False)
+        self.query = nn.Linear(emb_size, emb_size, bias=False)
+
+        self.relative_bias_table = nn.Parameter(torch.zeros((2 * self.seq_len - 1), num_heads))
+        coords = torch.meshgrid((torch.arange(1), torch.arange(self.seq_len)))
+        coords = torch.flatten(torch.stack(coords), 1)
+        relative_coords = coords[:, :, None] - coords[:, None, :]
+        relative_coords[1] += self.seq_len - 1
+        relative_coords = rearrange(relative_coords, 'c h w -> h w c')
+        relative_index = relative_coords.sum(-1).flatten().unsqueeze(1)
+        self.register_buffer("relative_index", relative_index)
+
+        self.dropout = nn.Dropout(dropout)
+        self.to_out = nn.LayerNorm(emb_size)
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.shape
+        k = self.key(x).reshape(batch_size, seq_len, self.num_heads, -1).permute(0, 2, 3, 1)
+        v = self.value(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
+        q = self.query(x).reshape(batch_size, seq_len, self.num_heads, -1).transpose(1, 2)
+        # k,v,q shape = (batch_size, num_heads, seq_len, d_head)
+
+        attn = torch.matmul(q, k) * self.scale
+        # attn shape (seq_len, seq_len)
+        attn = nn.functional.softmax(attn, dim=-1)
+
+        # Use "gather" for more efficiency on GPUs
+        relative_bias = self.relative_bias_table.gather(0, self.relative_index.repeat(1, self.num_heads))
+        relative_bias = rearrange(relative_bias, '(h w) c -> 1 c h w', h=1 * self.seq_len, w=1 * self.seq_len)
+        attn = attn + relative_bias
+
+        out = torch.matmul(attn, v)
+        # out.shape = (batch_size, num_heads, seq_len, d_head)
+        out = out.transpose(1, 2)
+        # out.shape == (batch_size, seq_len, num_heads, d_head)
+        out = out.reshape(batch_size, seq_len, -1)
+        # out.shape == (batch_size, seq_len, d_model)
+        out = self.to_out(out)
+        return out
+
+class ConvTran(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        # Parameters Initialization -------------------------------------------------
+        channel_size, seq_len = 64, 256
+        emb_size = 32
+        num_heads = 8
+        dim_ff = 2048
+        # Embedding Layer -----------------------------------------------------------
+        self.embed_layer = nn.Sequential(nn.Conv2d(1, emb_size*4, kernel_size=[1, 8], padding='same'),
+                                         nn.BatchNorm2d(emb_size*4),
+                                         nn.GELU())
+
+        self.embed_layer2 = nn.Sequential(nn.Conv2d(emb_size*4, emb_size, kernel_size=[channel_size, 1], padding='valid'),
+                                          nn.BatchNorm2d(emb_size),
+                                          nn.GELU())
+        
+        # Positional Encoding Layer -------------------------------------------------
+        self.Fix_Position = AbsPosEnc(emb_size, dropout=0.1, max_len=seq_len)
+        
+        # Attention Layer (with Relative positional encoding) -----------------------
+        self.attention_layer = AttentionWithRelPosEnc(emb_size, num_heads, seq_len, 0.15)
+       
+        self.LayerNorm = nn.LayerNorm(emb_size, eps=1e-5)
+        self.LayerNorm2 = nn.LayerNorm(emb_size, eps=1e-5)
+
+        # dropout has been set to 0.1 however it might need to be increased for better regularization
+        self.FeedForward = nn.Sequential(
+            nn.Linear(emb_size, dim_ff),
+            nn.ReLU(),
+            nn.Dropout(0.10),
+            nn.Linear(dim_ff, emb_size),
+            nn.Dropout(0.10))
+
+        self.gap = nn.AdaptiveAvgPool1d(1)
+        self.flatten = nn.Flatten()
+        self.out = nn.Linear(emb_size, num_classes)
+
+    def forward(self, x):
+        # create embeddings for time series data
+        x_src = self.embed_layer(x);
+        x_src = self.embed_layer2(x_src).squeeze(2)
+        x_src = x_src.permute(0, 2, 1)
+        # generate absolute positional encoding
+        x_src_pos = self.Fix_Position(x_src)
+        # pass through the attention layer and add skip connection (from embedding)
+        att = x_src + self.attention_layer(x_src_pos)
+        att = self.LayerNorm(att)
+        # pass through the feed forward layer and add skip connection (from attention)
+        out = att + self.FeedForward(att)
+        out = self.LayerNorm2(out)
+        out = out.permute(0, 2, 1)
+        out = self.gap(out)
+        out = self.flatten(out)
+        # linear layer for classification
+        out = self.out(out)
+        return out # one hot vector
